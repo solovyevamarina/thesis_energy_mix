7d62add6b787a489e8f4906df68b31f438903688
solovyevamarina <msol37115@gmail.com>
2024-02-12 20:14:22 +0200

    Data downloading overhaul

--- data_engineering/function_download.py
+++ data_engineering/function_download.py
@@ -3,40 +3,81 @@ import os.path
 from entsoe import EntsoePandasClient
 
 
-def entsoe_download(country, start, end, api_key, path):
-
-    # Download the data for specific country for a specific time period
-    # It takes quite some time
+def download_function(country, timezone, s_time, e_time, d_dataset, path, api_key):
 
     client = EntsoePandasClient(api_key=api_key)
-    gen_df = client.query_generation(country, start=start, end=end)
-    gen_df.to_csv(path + '/' + country + '_raw_' + start + '_' + end + '.csv')
-
-    return
-
-
-def download_data(country, timezone, s_time, e_time, d_dataset, path, api_key):
 
     # Transform dates to timestamps
     start_timestamp = pd.Timestamp(s_time, tz=timezone)
     end_timestamp = pd.Timestamp(e_time, tz=timezone)
 
-    # Set main name of dataset (country - type - start - end)
-    variable_name = f"{country}_raw_{start_timestamp.strftime('%Y%m%d')}_{end_timestamp.strftime('%Y%m%d')}"
-
-    # Downloading takes quite some time, so first check if it is already downloaded
-    file_path = path + '/' + variable_name + '.csv'
+    #---------------------------------------------------- Energy generation
+    file_name_gen = f"{country}_gen_{start_timestamp.strftime('%Y%m%d')}_{end_timestamp.strftime('%Y%m%d')}"
+    file_path = path + '/' + file_name_gen + '.csv'
     file_already_downloaded = os.path.isfile(file_path)
 
-    if file_already_downloaded:  # if already downloaded, read it and put into dictionary
-        print('Already downloaded', country, s_time, '-', e_time)
-        d_dataset[variable_name] = pd.read_csv(file_path)
-        print('Ready', country, s_time, '-', e_time)
-    else:  # otherwise, download, save in csv, read it and put into dictionary
-        print('Start downloading', country, s_time, '-', e_time)
-        entsoe_download(country, start_timestamp, end_timestamp, api_key, path)
+    if file_already_downloaded:
+        print('Already downloaded', file_name_gen)
+        d_dataset[file_name_gen] = pd.read_csv(file_path)
+
+    else:
+        print('Start downloading energy generation data', country, s_time, '-', e_time)
+        gen_df = client.query_generation(country, start=start_timestamp, end=end_timestamp)
+        gen_df.to_csv(path + '/' + country + '_gen_' + s_time + '_' + e_time + '.csv')
+        print('Finish downloading energy generation data', country, s_time, '-', e_time)
+
+    #---------------------------------------------------- Energy prices
+    file_name_price = f"{country}_price_{start_timestamp.strftime('%Y%m%d')}_{end_timestamp.strftime('%Y%m%d')}"
+    file_path = path + '/' + file_name_price + '.csv'
+    file_already_downloaded = os.path.isfile(file_path)
+
+    if file_already_downloaded:
+        print('Already downloaded', file_name_price)
+        d_dataset[file_name_price] = pd.read_csv(file_path)
+
+    else:
+        print('Start downloading energy prices data', country, s_time, '-', e_time)
+
+        if country == 'SE':
+            se_zones = ['SE_1', 'SE_2', 'SE_3', 'SE_4']
+            price_df = pd.DataFrame()
+            for zone in se_zones:
+                price_df_zone = client.query_day_ahead_prices(zone, start=start_timestamp, end=end_timestamp)
+                price_df = pd.concat([price_df, price_df_zone], axis=1)
+            price_df = price_df.sum(axis='columns')
+            price_df.to_csv(path + '/' + country + '_price_' + s_time + '_' + e_time + '.csv')
+            print('Finish downloading energy price data', country, s_time, '-', e_time)
+
+        else:
+            price_df = client.query_day_ahead_prices(country, start=start_timestamp, end=end_timestamp)
+            price_df.to_csv(path + '/' + country + '_price_' + s_time + '_' + e_time + '.csv')
+            print('Finish downloading energy price data', country, s_time, '-', e_time)
+
+    # ---------------------------------------------------- Energy load
+    file_name_load = f"{country}_load_{start_timestamp.strftime('%Y%m%d')}_{end_timestamp.strftime('%Y%m%d')}"
+    file_path = path + '/' + file_name_load + '.csv'
+    file_already_downloaded = os.path.isfile(file_path)
+
+    if file_already_downloaded:
+        print('Already downloaded', file_name_load)
+        d_dataset[file_name_load] = pd.read_csv(file_path)
+
+    else:
+        print('Start downloading energy load data', country, s_time, '-', e_time)
+        if country == 'SE':
+            se_zones = ['SE_1', 'SE_2', 'SE_3', 'SE_4']
+            price_df = pd.DataFrame()
+            for zone in se_zones:
+                price_df_zone = client.query_day_ahead_prices(zone, start=start_timestamp, end=end_timestamp)
+                price_df = pd.concat([price_df, price_df_zone], axis=1)
+            price_df = price_df.sum(axis='columns')
+            price_df.to_csv(path + '/' + country + '_load_' + s_time + '_' + e_time + '.csv')
+        else:
+            price_df = client.query_day_ahead_prices(country, start=start_timestamp, end=end_timestamp)
+            price_df.to_csv(path + '/' + country + '_load_' + s_time + '_' + e_time + '.csv')
+
+        # TTF prices already exist in the folder
+
         print('Finished downloading', country, s_time, '-', e_time)
-        d_dataset[variable_name] = pd.read_csv(file_path)
-        print('Ready', country, s_time, '-', e_time)
 
     return d_dataset
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-from data_engineering.function_download import download_data
+from data_engineering.function_download import download_function
 
 ############################################### KEY PARAMETERS #########################################################
 
@@ -12,16 +12,15 @@ time_zones = ['Europe/Athens', 'Europe/Warsaw', 'Europe/Stockholm']
 start_time = '20150101'
 end_time = '20221101'
 
-my_api_key = 'my_api_key'
+my_api_key = '611caff8-769f-4d12-8a5e-838efb1f684a'
 my_path = 'E:/Study/Diploma/csv'
-
-raw_datasets = {}
 
 ################################################ DOWNLOAD DATA #########################################################
 
-# Data will be saved in csv format in the path folder
-# It takes quite some time (several hours depending on the time window)
-# If file already exists, read pre-downloaded data
+raw_datasets = {}
 
 for country, timezone in zip(countries, time_zones):
-    download_data(country, timezone, start_time, end_time, raw_datasets, my_path, my_api_key)
+    download_function(country, timezone, start_time, end_time, raw_datasets, my_path, my_api_key)
+
+################################################## CLEAN DATA ##########################################################
+
